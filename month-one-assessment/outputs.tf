7440b78611fe81dbea53828edc05820a4b3f79b8
BullionMoney09 <emmanuelbullion9@gmail.com>
2026-03-14 15:51:13 +0100

    Update outputs.tf

--- month-one-assessment/outputs.tf
+++ month-one-assessment/outputs.tf
@@ -3,12 +3,12 @@ output "vpc_id" {
   value       = aws_vpc.techcorp_vpc.id
 }
 
-output "load_balancer_dns" {
-  description = "DNS name of the Application Load Balancer"
-  value       = aws_lb.alb.dns_name
+output "bastion_public_ip" {
+  description = "Public IP of the bastion host"
+  value       = aws_instance.bastion.public_ip
 }
 
-output "bastion_public_ip" {
-  description = "Public IP address of the Bastion Host"
-  value       = aws_instance.bastion.public_ip
+output "load_balancer_dns" {
+  description = "DNS name of the load balancer"
+  value       = aws_lb.techcorp_alb.dns_name
 }
