7bcb84228cb12fa66bc31ad610e72733bec2ae51
BullionMoney09 <emmanuelbullion9@gmail.com>
2026-03-14 16:48:19 +0100

    Refactor outputs in outputs.tf for clarity
    Updated output values and descriptions for bastion host and load balancer.

--- month-one-assessment/outputs.tf
+++ month-one-assessment/outputs.tf
@@ -1,14 +1,9 @@
-output "vpc_id" {
-  description = "ID of the VPC"
-  value       = aws_vpc.techcorp_vpc.id
-}
-
 output "bastion_public_ip" {
-  description = "Public IP of the bastion host"
-  value       = aws_instance.bastion.public_ip
+  description = "Public IP of Bastion"
+  value       = aws_instance.bastion_host.public_ip
 }
 
 output "load_balancer_dns" {
-  description = "DNS name of the load balancer"
+  description = "ALB DNS Name"
   value       = aws_lb.techcorp_alb.dns_name
 }
