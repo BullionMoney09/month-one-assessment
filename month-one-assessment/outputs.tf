2414bae6dc8d2d518f42bed41c5a80f520227d4f
BullionMoney09 <emmanuelbullion9@gmail.com>
2026-03-14 16:57:27 +0100

    Update outputs.tf

--- month-one-assessment/outputs.tf
+++ month-one-assessment/outputs.tf
@@ -1,9 +1,9 @@
 output "bastion_public_ip" {
-  description = "Public IP of Bastion"
+  description = "Public IP of the Bastion host"
   value       = aws_instance.bastion_host.public_ip
 }
 
 output "load_balancer_dns" {
-  description = "ALB DNS Name"
+  description = "DNS name of the Application Load Balancer"
   value       = aws_lb.techcorp_alb.dns_name
 }
