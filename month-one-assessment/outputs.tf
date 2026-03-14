1c8d953b639712df29a21f3b0adb3b6816001b21
BullionMoney09 <emmanuelbullion9@gmail.com>
2026-03-14 15:02:50 +0100

    Refactor outputs in outputs.tf for clarity

--- month-one-assessment/outputs.tf
+++ month-one-assessment/outputs.tf
@@ -1,9 +1,14 @@
-output "bastion_public_ip" {
-  description = "Public IP of bastion host"
-  value = aws_instance.bastion.public_ip
+output "vpc_id" {
+  description = "ID of the VPC"
+  value       = aws_vpc.techcorp_vpc.id
 }
 
-output "alb_dns" {
-  description = "Load balancer DNS"
-  value = aws_lb.alb.dns_name
+output "load_balancer_dns" {
+  description = "DNS name of the Application Load Balancer"
+  value       = aws_lb.alb.dns_name
+}
+
+output "bastion_public_ip" {
+  description = "Public IP address of the Bastion Host"
+  value       = aws_instance.bastion.public_ip
 }
